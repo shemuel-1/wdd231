1cdf301ba29488868af2746248ebbc620d327068
shemuel-1 <samuelpaintsil246@gmail.com>
2025-09-13 22:18:18 +0000

    completed chamber directory page mobile view

---
+++ chamber/scripts/scripts.js
@@ -0,0 +1,53 @@
+// ******************** NAVIGATION *********************
+
+const navButton = document.getElementById("ham-button");
+const navMenu = document.getElementById("nav-bar");
+
+navButton.addEventListener("click", () => {
+    navButton.classList.toggle("show");
+    navMenu.classList.toggle("show");
+});
+
+
+async function displayBusinessCards() {
+  try {
+    const response = await fetch('data/members.json');
+    const members = await response.json();
+    const businessesSection = document.getElementById('businesses');
+    businessesSection.innerHTML = "";
+
+    members.forEach(member => {
+      // Create card container
+      const card = document.createElement('div');
+        card.classList.add('business-card');
+
+      // Fill card with member info 
+        card.innerHTML = `
+        <img src="${member.image}" alt="${member.name} image" width="280">
+        <h3>${member.name}</h3>
+        <p><strong>Address: </strong>${member.address}</p>
+        <p><strong>Phone: </strong>${member.phone}</p>
+        <p><strong>Industry: </strong>${member.industry}</p>
+        <p><a href="${member.website}" target="_blank">Website</a></p>
+      `;
+
+      // Add card to section
+      businessesSection.appendChild(card);
+    });
+  } catch (error) {
+    console.error('Error loading business cards:', error);
+  }
+}
+
+displayBusinessCards();
+
+
+const listButton = document.getElementById("layout");
+
+listButton.addEventListener("click", () => {
+    const businesses = document.querySelectorAll(".business-card");
+
+    businesses.forEach(business => {
+        business.classList.toggle("list");
+    });
+});
